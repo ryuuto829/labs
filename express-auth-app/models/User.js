fa9c6078ececb7c1f22730e9839a7a12de76d03e
ryuuto829 <ryuuto829@gmail.com>
2021-02-11 20:49:04 +0200

    feat: add views for login and signup, update user schema

--- express-auth-app/models/User.js
+++ express-auth-app/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,16 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Enter a password'],
     minlength: [6, 'Password should be at least 6 characters in length'],
   },
+});
+
+// Fire some function before document saved to database
+// eslint-disable-next-line prettier/prettier
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 const User = mongoose.model('user', userSchema);
