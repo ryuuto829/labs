1a23f8dae13ece17caeea525d7b153d67800fe9c
ryuuto829 <ryuuto829@gmail.com>
2021-03-08 15:10:33 +0200

    feat: add mongobd limiter

--- node-mini-blog/routes/blog.js
+++ node-mini-blog/routes/blog.js
@@ -7,13 +7,31 @@ const {
   blogDelete,
 } = require('../controllers/blog');
 
+const RateLimit = require('express-rate-limit');
+const MongoStore = require('rate-limit-mongo');
+require('dotenv').config();
+
+const { DB_URI } = process.env;
+
+const rateLimitDelay = 30 * 60 * 1000; // 30 minutes delay
+const limiter = new RateLimit({
+  store: new MongoStore({
+    uri: DB_URI,
+    expireTimeMs: rateLimitDelay,
+  }),
+  max: 25,
+  windowMs: rateLimitDelay,
+  message: "Too many actions were created from this IP, please try again after a 30 minutes."
+});
+
+
 const router = express.Router();
 
 // Routes for all blogs
 router.get('/', blogIndex);
-router.post('/', blogCreatePost);
-router.get('/create', blogCreate);
+router.post('/', limiter, blogCreatePost);
+router.get('/create', limiter, blogCreate);
 router.get('/:id', blogDetails);
-router.delete('/:id', blogDelete);
+router.delete('/:id', limiter, blogDelete);
 
 module.exports = router;
